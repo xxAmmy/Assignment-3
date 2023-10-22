16d0cc713ce66c08e2192d8d755a13b3d6abe751
xxAmmy <147511889+xxAmmy@users.noreply.github.com>
2023-10-22 16:33:43 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -21,31 +21,40 @@ class MemoryTestWindow:
         window.title('Memory Test')
         window.minsize(200, 200)
 
+        self.font = ('sans 20 bold')
+
         #top label
-        self.__start_label = Label(text = "click 'Start' to begin the memory test", font = ('Arial bold', 20))
+        self.__start_label = Label(text = "click 'Start' to begin the memory test", font = self.font)
         self.__start_label.pack()
 
         #make canvas
-        self.__canvas = Canvas(root, width = 1200, height = 700, bg = 'white')
+        self.__canvas = Canvas(root, width = 1200, height = 700)
         self.__canvas.pack()
 
         #bottom text
         toolbar = Frame(window)
-        self.__startbutton = Button(toolbar, text = 'start', font = ("Arial bold", 20), command = self.count_down)
+        #start button
+        self.__startbutton = Button(toolbar, text = 'start', font = self.font, command= self.count_down)
         self.__startbutton.pack(side = 'left')
-        label1 = Label(toolbar, text = 'ms invisible:', font = ("Arial bold", 20))
+        #ms invisible
+        label1 = Label(toolbar, text = 'ms invisible:', font = self.font)
         label1.pack(side = 'left')
-        self.__entry1 = Entry(toolbar, font = ('Arial bold', 20))
+        #entry from ms invisible
+        self.__entry1 = Entry(toolbar, font = self.font)
         self.__entry1.insert(0, "500")
         self.__entry1.pack(side = 'left')
-        label2 = Label(toolbar, text = 'ms between:', font = ("Arial bold", 20))
+        #ms between
+        label2 = Label(toolbar, text = 'ms between:', font = self.font)
         label2.pack(side = 'left')
-        self.__entry2 = Entry(toolbar, font = ("Arial bold", 20))
+        #entry ms between
+        self.__entry2 = Entry(toolbar, font = self.font)
         self.__entry2.pack(side = 'left')
         self.__entry2.insert(0, "500")
-        label3 = Label(toolbar, text = 'sequence length:', font = ("Arial bold", 20))
+        #sequence length
+        label3 = Label(toolbar, text = 'sequence length:', font = self.font)
         label3.pack(side = 'left')
-        self.__entry3 = Entry(toolbar, font = ("Arial bold", 20))
+        #entry sequence length
+        self.__entry3 = Entry(toolbar, font = self.font)
         self.__entry3.insert(0, "3")
         self.__entry3.pack(side = 'left')
         toolbar.pack(side=BOTTOM)
@@ -63,81 +72,59 @@ class MemoryTestWindow:
         else: #3. start obervation phase
             self.observation()
 
-    def observation(self): #hier zou je root bij moeten zetten voor de code hieronder maar dan werkt het niet meer
-        self.__start_label.config(text = "watch the sequence...", font = ('Arial bold', 20))
-        self.__start_label.pack()
+    def show(self, square):
+        self.__canvas.itemconfigure(square, state="normal")
 
-        blue_square = self.__canvas.create_rectangle(280, 30, 580, 330, fill = "blue", tags = "blue")
-        red_square = self.__canvas.create_rectangle(620, 30, 920, 330, fill = "red", tags = "red")
-        green_square = self.__canvas.create_rectangle(280, 370, 580, 670, fill = "green", tags = "green")
-        yellow_square = self.__canvas.create_rectangle(620, 370, 920, 670, fill = "yellow", tags = "yellow")
-        
-        for _ in range(self.__entry3):
-            random_color = randint(1,4)
-            if random_color == 1:
-                def hide_blue(root, blue_square):
-                    self.__canvas.delete(blue_square)
-                root.after(2000, hide_blue) #ipv 2000 dan de self.__entry1
-            if random_color == 2:
-                def hide_red(root, red_square):
-                    self.__canvas.delete(red_square)
-                root.after(2000, hide_red)
-            if random_color == 3:
-                def hide_green(root, green_square):
-                    self.__canvas.delete(green_square)
-                root.after(2000, hide_green)
-            if random_color == 4:
-                def hide_yellow(root, yellow_square):
-                    self.__canvas.delete(yellow_square)
-                root.after(2000, hide_yellow)
+    def hide(self, square):
+        self.__canvas.itemconfigure(square, state="hidden")
 
-    #hier is de code bewerkt
-    #for de range (self.__entry3) wordt self.__entry3 niet gezien als een integer ook niet als ik zet int(self.__entry3)
-    #dus vandaar dat ik hem hier als getal 3 heb weergegeven
-    #je hebt nu voor elke sqaure een aparte functie gemaakt voor verdwijnen maar je kon ook een fucntie met --> def hide_square(self, sqaure), maken ik heb dat gedaan en toen kon hij de after functie niet
-    #toepassen omdat je de after functie alleen kan gebruiken voor een root en niet een self (idk wat dat betekent maar die error kreeg ik)
+    def observation(self):
+        k = True
+        while k == True:
+            self.__start_label.config(text = "watch the sequence...", font = self.font)
+            self.__start_label.pack()
 
-        for _ in range(3):
-            random_color = randint(1, 4)
-            if random_color == 1:
-                self.__canvas.after(2000, lambda: self.__canvas.delete(blue_square))
-            elif random_color == 2:
-                self.__canvas.after(2000, lambda: self.__canvas.delete(red_square))
-            elif random_color == 3:
-                self.__canvas.after(2000, lambda: self.__canvas.delete(green_square))
-            elif random_color == 4:
-                self.__canvas.after(2000, lambda: self.__canvas.delete(yellow_square))
+            blue_square = self.__canvas.create_rectangle(280, 30, 580, 330, fill = "blue", tags = "blue")
+            red_square = self.__canvas.create_rectangle(620, 30, 920, 330, fill = "red", tags = "red")
+            green_square = self.__canvas.create_rectangle(280, 370, 580, 670, fill = "green", tags = "green")
+            yellow_square = self.__canvas.create_rectangle(620, 370, 920, 670, fill = "yellow", tags = "yellow")
+            
+            sequence_length = int(self.__entry3.get())
+            ms_between = int(self.__entry2.get())
+            ms_invisible = int(self.__entry1.get())
+            loop_time = ms_between + ms_invisible
+            sequence = []
 
-    #hij doet het nu alleen nog maar 1 keer, maar wel maar 1 vierkantje tegelijk die verdwijnt en dan terugkomt
-    #kijk eens naar 8.6 daar is een beetje hetzelfde proces en daarom was ik een beetje aan begonnen
+            for i in range(sequence_length):
+                random_color = randint(1, 4)
+                t = ms_between
+                back = t + i*loop_time + ms_invisible
+                
 
-        sequence_length = int(self.__entry3.get())
-        ms_between = int(self.__entry2.get())
-        ms_invisible = int(self.__entry1.get())
-        nr_dissapear = 0
+                if random_color == 1:
+                    self.__canvas.after(t + i*loop_time, lambda: self.hide(blue_square))
+                    self.__canvas.after(back, lambda: self.show(blue_square))
+                    sequence.append('1')
+                elif random_color == 2:
+                    self.__canvas.after(t + i*loop_time, lambda: self.hide(red_square))
+                    self.__canvas.after(back, lambda: self.show(red_square))
+                    sequence.append('2')
+                elif random_color == 3:
+                    self.__canvas.after(t + i*loop_time, lambda: self.hide(green_square))
+                    self.__canvas.after(back, lambda: self.show(green_square))
+                    sequence.append('3')
+                elif random_color == 4:
+                    self.__canvas.after(t + i*loop_time, lambda: self.hide(yellow_square))
+                    self.__canvas.after(back, lambda: self.show(yellow_square))
+                    sequence.append('4')
+            k= FALSE
 
-        #for _ in range(sequence_length):
-        if nr_dissapear < sequence_length:
-            random_color = randint(1, 4)
-            if random_color == 1:
-                self.__canvas.after(ms_between, lambda: self.__canvas.delete(blue_square))
-                self.__canvas.after(2000, lambda: self.__canvas.create_rectangle(280, 30, 580, 330, fill = "blue", tags = "blue"))
-                nr_dissapear = nr_dissapear +1
-            elif random_color == 2:
-                self.__canvas.after(ms_between, lambda: self.__canvas.delete(red_square))
-                self.__canvas.after(2000, lambda: self.__canvas.create_rectangle(620, 30, 920, 330, fill  = "red", tags = "red"))
-                nr_dissapear = nr_dissapear +1
-            elif random_color == 3:
-                self.__canvas.after(ms_between, lambda: self.__canvas.delete(green_square))
-                self.__canvas.after(2000, lambda: self.__canvas.create_rectangle(280, 370, 580, 670, fill = "green", tags = "green"))
-                nr_dissapear = nr_dissapear +1
-            elif random_color == 4:
-                self.__canvas.after(ms_between, lambda: self.__canvas.delete(yellow_square))
-                self.__canvas.after(2000, lambda: self.__canvas.create_rectangle(620, 370, 920, 670, fill = "yellow", tags = "yellow"))
-                nr_dissapear = nr_dissapear +1
+        else:
+            self.sequence()
+            
+        #sequence list is heel waarschijnlijk nodig voor opdracht 6 vandaar dat ik hem heb laten staan want ik had m ook gebruikt om de code te testen en bekijken
     
-    
-    def sequence(self):
+    def sequence(self): #opdracht 5
         squares = []
         squares.append(self.__canvas.create_rectangle(280, 30, 580, 330, fill = "blue"))
         squares.append(self.__canvas.create_rectangle(620, 30, 920, 330, fill = "red"))
